1b2bb56fae49de210aa186b10e34dd748e31b954
T4533N <taseentanvir@gmail.com>
2022-03-02 19:15:56 +0600

    .

---
+++ components/Chevron.tsx
@@ -0,0 +1,22 @@
+function Chevron({ style }: any) {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className={`w-6 h-6 ${style}`}
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M5 15l7-7 7 7"
+        />
+      </svg>
+    </div>
+  );
+}
+
+export default Chevron;
