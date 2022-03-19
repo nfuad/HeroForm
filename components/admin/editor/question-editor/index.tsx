14c4b2046c09b9a202ae78b46424f62ec6f52322
Andrew Cen <werdna521@gmail.com>
2022-03-19 19:49:30 +0700

    update ui

--- components/admin/editor/question-editor/index.tsx
+++ components/admin/editor/question-editor/index.tsx
@@ -29,7 +29,7 @@ const QuestionEditor: FC<Props> = ({ question, updateSelectedQuestion }) => {
           <QuestionPrompt prompt={question.prompt} onChange={setPrompt} />
           {isShortText && (
             <input
-              className="w-full max-w-3xl mx-auto mt-8 text-2xl px-7 py-7 font-heading rounded-3xl placeholder:text-gray-300 placeholder:font-body"
+              className="flex-shrink-0 w-full max-w-3xl mx-auto mt-8 text-2xl px-7 py-7 font-heading rounded-3xl placeholder:text-gray-300 placeholder:font-body"
               placeholder={question.placeholder}
               type="text"
               style={{
@@ -40,11 +40,12 @@ const QuestionEditor: FC<Props> = ({ question, updateSelectedQuestion }) => {
           )}
           {isLongText && (
             <textarea
-              className="w-full max-w-3xl mx-auto mt-8 text-2xl resize-none px-7 py-7 font-heading rounded-3xl placeholder:text-gray-300 placeholder:font-body"
+              className="flex-shrink-0 w-full max-w-3xl mx-auto mt-8 text-2xl resize-none px-7 py-7 font-heading rounded-3xl placeholder:text-gray-300 placeholder:font-body"
               placeholder={question.placeholder}
               style={{
                 boxShadow: '0px 14px 39px 10px rgba(235, 145, 145, 0.2)',
               }}
+              rows={5}
               disabled
             />
           )}
--- components/admin/editor/question-editor/options.tsx
+++ components/admin/editor/question-editor/options.tsx
@@ -7,7 +7,12 @@ type Props = {
   setOptions: (options: Option[]) => void
 }
 
+const MAX_OPTIONS = 10
+const CHAR_CODE_OFFSET = 65
+
 const Options: FC<Props> = ({ options, setOptions }) => {
+  const optionsLimitReached = options.length === MAX_OPTIONS
+
   const handleChange =
     (id: string): ChangeEventHandler<HTMLInputElement> =>
     (e) => {
@@ -20,38 +25,59 @@ const Options: FC<Props> = ({ options, setOptions }) => {
       )
     }
 
+  const handleDelete = (id: string) => () => {
+    setOptions(options.filter((option) => option.id !== id))
+  }
+
   const handleAdd = () => {
     setOptions([...options, createOption()])
   }
 
   const renderOptions = () => {
-    return options.map(({ id, value }) => (
+    return options.map(({ id, value }, index) => (
       <div
         key={id}
-        className="flex items-center bg-white border-2 border-zinc-800 gap-x-3 w-max min-w-[384px] p-3 rounded-xl focus-within:ring-4 focus-within:ring-blue-100 focus-within:border-blue-400 transition duration-300"
+        className="group flex items-center bg-white border-2 border-zinc-800 gap-x-3 w-max min-w-[384px] p-3 rounded-xl focus-within:ring-4 focus-within:ring-blue-100 focus-within:border-blue-400 transition duration-300"
       >
-        <div className="px-2 text-2xl font-bold text-gray-700 border-2 rounded-lg border-zinc-800 font-body">
-          A
+        <div className="flex items-center justify-center flex-shrink-0 text-2xl font-bold text-gray-700 border-2 rounded-lg w-9 h-9 border-zinc-800 font-body">
+          {String.fromCharCode(index + CHAR_CODE_OFFSET)}
         </div>
         <input
           className="w-full text-2xl font-semibold text-gray-700 font-body focus:outline-none"
           value={value}
           onChange={handleChange(id)}
+          placeholder="Option"
         />
+        <button type="button" onClick={handleDelete(id)}>
+          <svg
+            className="w-6 h-6 text-gray-700 transition duration-300 opacity-0 stroke-current hover:text-red-500 group-hover:opacity-100"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
       </div>
     ))
   }
 
   return (
-    <div className="flex flex-col items-center mx-auto mt-8 gap-y-4">
+    <div className="flex flex-col items-center flex-shrink-0 mx-auto mt-8 gap-y-4">
       {renderOptions()}
-      <button
-        type="button"
-        className="mt-4 text-xl font-semibold text-gray-700 transition duration-300 font-body hover:text-gray-900 focus:outline-none"
-        onClick={handleAdd}
-      >
-        + Add option
-      </button>
+      {!optionsLimitReached && (
+        <button
+          type="button"
+          className="mt-4 text-xl font-semibold text-gray-700 transition duration-300 font-body hover:text-gray-900 focus:outline-none"
+          onClick={handleAdd}
+        >
+          + Add option
+        </button>
+      )}
     </div>
   )
 }
