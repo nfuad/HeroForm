13b8ee62faf3b92281d116bba6d0a32b3b5ab914
nafis <24988773+nfuad@users.noreply.github.com>
2022-07-12 14:01:00 +0600

    add try catch

--- hooks/use-plausible.ts
+++ hooks/use-plausible.ts
@@ -14,11 +14,15 @@ type EventOptionsTuple<P extends Props> = P extends never
 type Events = { [K: string]: Props }
 
 export function usePlausible<E extends Events = any>() {
-  return useCallback(function <N extends keyof E>(
-    eventName: N,
-    ...rest: EventOptionsTuple<E[N]>
-  ) {
-    return (window as any).plausible?.(eventName, rest[0])
-  },
-  [])
+  try {
+    return useCallback(function <N extends keyof E>(
+      eventName: N,
+      ...rest: EventOptionsTuple<E[N]>
+    ) {
+      return (window as any).plausible?.(eventName, rest[0])
+    },
+    [])
+  } catch (error) {
+    console.log(error)
+  }
 }
